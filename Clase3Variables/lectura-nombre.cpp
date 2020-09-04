f2d099f2fe6db9e99087271b64f4b72e7b451a05
maajaragi <maajaragi@unal.edu.co>
2020-09-04 18:20:14 -0500

    ejemplo lectura nombre

---
+++ Clase3Variables/lectura-nombre.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+  std::string fullname;
+  std::cout <<"Hola, escribe tu nombre, por favor:\n";
+  std::getline(std::cin, fullname);
+  std::cout <<"Hola," << fullname << std::endl;
+
+  return 0;
+}
