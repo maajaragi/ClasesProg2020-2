4ce970bfbf94144ede8ee2805141b93a25ececd0
maajaragi <maajaragi@unal.edu.co>
2020-09-04 06:50:22 -0500

    inserción función seno

--- Clase2Git/ejemplo.cpp
+++ Clase2Git/ejemplo.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+//Primer ejemplo de C++
+
+#include <iostream> //Incluye libreria estandar
+#include <cmath>    //Incluye funciones matemáticas
 
 int main(void)
 {
   std::cout << "Hello World!\n";
   std::cout << "Hola Mundo!\n";
+  std::cout << "sin (90):" << std::sin (90)<<"\n";
   return 0;
 }
