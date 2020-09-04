e7e412fcc04d84a71a9a4262c31b6f55b3a49741
maajaragi <maajaragi@unal.edu.co>
2020-09-04 17:45:16 -0500

    ejemplo argc argv

---
+++ Clase3Variables/ejemplo1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char **argv)
+{
+  std::cout << argc <<std::endl;
+  std::cout <<argv [0] <<std::endl;
+  std::cout <<argv [1] <<std::endl;
+  std::cout <<argv [2] <<std::endl;
+  return 0;
+}
